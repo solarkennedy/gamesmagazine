4c9798c38c7d0ada9b0a8e7e792ce25c8f12d839
kyle <kyle@c1d18bf3-4d93-4f84-9f4c-9e37f7f76c97>
2011-03-16 01:42:25 +0000

    added list solution

--- Flying High/flyinghigh-lists.py
+++ Flying High/flyinghigh-lists.py
@@ -17,32 +17,32 @@
 # 14 15 16 17
 #    18
 
-from sets import Set
-chipmunks = Set([1,7,12,13,15])
-monkeys   = Set([2,11,14,16])
-bears     = Set([3,5,8,17])
-foxes     = Set([4,6,10,18])
-animals = Set([chipmunks,monkeys,bears,foxes])
+chipmunks = [1,7,12,13,15]
+monkeys   = [2,11,14,16]
+bears     = [3,5,8,17]
+foxes     = [4,6,9,10,18]
+animals = [chipmunks,monkeys,bears,foxes]
 
-blues     = Set([1,3,14,17,18])
-browns    = Set([2,4,7,9,10,16])
-greys     = Set([6,8,11,13,15])
-colors = Set([blues,browns,greys])
+blues     = [1,3,14,17,18]
+browns    = [2,4,7,9,10,16]
+greys     = [6,8,11,13,15]
+pinks	  = [5,12]
+colors = [blues,browns,greys,pinks]
 
 #There might be a better way to do this, but I'm sticking with sets:
-row1 = Set([1])
-row2 = Set([2,3,4,5])
-row3 = Set([6,7,8,9])
-row4 = Set([10,11,12,13])
-row5 = Set([14,15,16,17])
-row6 = Set([18])
-rows = Set([row1,row2,row3,row4,row5,row6])
+row1 = [1]
+row2 = [2,3,4,5]
+row3 = [6,7,8,9]
+row4 = [10,11,12,13]
+row5 = [14,15,16,17]
+row6 = [18]
+rows = [row1,row2,row3,row4,row5,row6]
 
-col1 = Set([2,6,10,14])
-col2 = Set([3,7,11,15,18])
-col3 = Set([1,4,8,12,16])
-col4 = Set([5,9,13,17])
-cols = Set([col1,col2,col3,col4])
+col1 = [2,6,10,14]
+col2 = [3,7,11,15,18]
+col3 = [1,4,8,12,16]
+col4 = [5,9,13,17]
+cols = [col1,col2,col3,col4]
 
 def Recurse(Position,PathTaken):
    if Position == 1:
@@ -53,19 +53,19 @@ def Recurse(Position,PathTaken):
       return
    else: 
       # Lets go through our sets and see what our attributes are
-      mycolorset = [color for color in colors if Position in color]
-      myrow = [animal for animal in animals if Position in animal]
-      myrow = [row for row in rows if Position in row]
-      mycol = [col for col in cols if Position in col]
+      mycolor = [color for color in colors if Position in color][0]
+      myanimal = [animal for animal in animals if Position in animal][0]
+      myrow = [row for row in rows if Position in row][0]
+      mycol = [col for col in cols if Position in col][0]
       #For possible spaces in my row
       for Possibility in myrow:
          #We are allowed to move if it is the same color or same animal
-         if Possibility mycolor or myanimal == animalsdict[Possibility] and Possibility not in PathTaekn:
+         if (Possibility in mycolor or Possibility in myanimal) and Possibility not in PathTaken:
             Recurse(Possibility, PathTaken + (Possibility,))
       #For Possible spaces in our same column
-      for Possibility in [(k) for k, v in coldict.items() if v==mycol and k not in PathTaken]:
+      for Possibility in mycol:
          #We are allowed to move if it is the same color or same animal
-         if mycolor == colorsdict[Possibility] or myanimal == animalsdict[Possibility]:
+         if (Possibility in mycolor or Possibility in myanimal) and Possibility not in PathTaken:
             Recurse(Possibility, PathTaken + (Possibility,))
 
 #Starting Position 18, go!
