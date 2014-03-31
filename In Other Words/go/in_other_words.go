7c2ebf9cc5ef86f8766f11baa3054ed856ae2db0
Kyle Anderson <kyle@xkyle.com>
2014-03-30 19:57:44 -0700

    More recursive function for searching for words

--- In Other Words/go/in_other_words.go
+++ In Other Words/go/in_other_words.go
@@ -4,6 +4,7 @@ import "fmt"
 import "bufio"
 import "os"
 import "strings"
+import "regexp"
 
 func main() {
 	hints := Read_hints_file()
@@ -11,13 +12,13 @@ func main() {
 	for _, hint := range hints {
 		fmt.Print("Trying to find words that match: ")
 		fmt.Println(hint)
-		answers := Search_for_word(hint, words)
+		_, answers := Search_for_word(hint, words)
 		fmt.Println("Answers: ")
 		fmt.Println(answers)
 	}
 }
 
-func Import_wordlist() ([]string) {
+func Import_wordlist() []string {
 	words := []string{}
 	file, _ := os.Open("/usr/share/dict/words")
 	scanner := bufio.NewScanner(file)
@@ -27,10 +28,10 @@ func Import_wordlist() ([]string) {
 	return words
 }
 
-func Read_hints_file() ([][]string) {
+func Read_hints_file() [][]string {
 	hints := [][]string{}
 	file, _ := os.Open("hints.txt")
-        scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		hint := strings.Split(scanner.Text(), ", ")
 		hints = append(hints, hint)
@@ -38,6 +39,24 @@ func Read_hints_file() ([][]string) {
 	return hints
 }
 
-func Search_for_word(hint []string, words[]string) ([]string) {
-	return []string{ "Bogus answer" }
+func Search_for_word(hint_words []string, words []string) ([]string, []string) {
+	if len(hint_words) == 0 {
+		fmt.Println("DEBUG: Out of hints")
+		return []string{}, []string{}
+	} else if len(hint_words) == 0 {
+		fmt.Println("DEBUG: Out of hints")
+		return []string{}, []string{}
+	} else {
+		word := hint_words[0]
+		fmt.Println("Searching all words for " + word)
+		matching_words := []string{}
+		word_regex := word
+		for _, dict_word := range words {
+			match, _ := regexp.MatchString(word_regex, dict_word)
+			if match {
+				matching_words = append(matching_words, dict_word)
+			}
+		}
+		return Search_for_word(hint_words[1:], matching_words)
+	}
 }
