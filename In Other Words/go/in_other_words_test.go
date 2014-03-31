8caca5b739fbe259c3c5ffa76eb42613a0810f0e
Kyle Anderson <kyle@xkyle.com>
2014-03-30 19:57:57 -0700

    go fmt

--- In Other Words/go/in_other_words_test.go
+++ In Other Words/go/in_other_words_test.go
@@ -2,13 +2,12 @@ package main
 
 import "testing"
 import "reflect"
-//import "fmt"
 
 func TestRead_hints_file(t *testing.T) {
-	hints :=  Read_hints_file()
-	expected := []string{  "ANT", "CHAT", "ERA", "MEAT", "RAN"  }
+	hints := Read_hints_file()
+	expected := []string{"ANT", "CHAT", "ERA", "MEAT", "RAN"}
 	actual := hints[0]
-	if ! reflect.DeepEqual(actual, expected) {
+	if !reflect.DeepEqual(actual, expected) {
 		t.Log(actual)
 		t.Log(expected)
 		t.Error("Actual and expected differ ^")
@@ -16,19 +15,19 @@ func TestRead_hints_file(t *testing.T) {
 }
 
 func TestSearch_for_word(t *testing.T) {
-	hint := []string{  "ANT", "CHAT", "ERA", "MEAT", "RAN"  }
-	words := []string{ "bogus", "test_string", "merchant", "merchants" }
+	hint := []string{"ANT", "CHAT", "ERA", "MEAT", "RAN"}
+	words := []string{"bogus", "test_string", "merchant", "merchants"}
 	actual := Search_for_word(hint, words)
-	expected := []string{ "merchant", "merchants"  }
-	if ! reflect.DeepEqual(actual, expected) {
-                t.Log(actual)
-                t.Log(expected)
-                t.Error("Actual and expected differ ^")
-        }
+	expected := []string{"merchant", "merchants"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Log(actual)
+		t.Log(expected)
+		t.Error("Actual and expected differ ^")
+	}
 }
 
 func BenchmarkImport_wordlist(b *testing.B) {
-        for n := 0; n < b.N; n++ {
+	for n := 0; n < b.N; n++ {
 		Import_wordlist()
-        }
+	}
 }
